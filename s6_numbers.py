d5c96001119cc59d92784632c8d307e71b3a9791
Josué Clement <JosueC@nid.swiss>
2020-10-13 15:32:51 +0200

    Add files

---
+++ s6_numbers.py
@@ -0,0 +1,28 @@
+# Two main types of numbers :
+#   Integer -> 4 / 57 / -10
+#   Floating Point 6.1 / 1.3333 / 0.0
+
+# int + int -> int
+# int + float -> float
+# int - int -> int
+# int - float -> float
+# int * int -> int
+# int * float -> float
+# int / int -> float
+
+# Commonly used math operators :
+#   +   -> Addition
+#   -   -> Substraction
+#   *   -> Multiplication
+#   /   -> Division
+#   **  -> Exponentiation
+#   %   -> Modulo
+#   //  -> Integer Division
+
+# Operators orders :
+#   Parentheses
+#   Exponents
+#   Multiplication
+#   Division
+#   Adition
+#   Substraction
---
+++ s7_variables_and_strings.py
@@ -0,0 +1,5 @@
+x = 100 # Define a new variable
+print(x) # Prints the variable x
+
+var1, var2, var3 = 1, 99, 3 # Define multiple variables at once
+print(var1, var2, var3) # Prints the variables
