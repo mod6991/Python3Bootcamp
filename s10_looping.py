3747e147d95c169c6d1f89bf5404fc86696c0e48
Josué Clement <JosueC@nid.swiss>
2020-10-14 15:04:04 +0200

    save changes

---
+++ s10_looping.py
@@ -0,0 +1,27 @@
+# --------
+# for loop
+# --------
+
+for x in range(1,10): #prints 1 to 9
+    print(x)
+
+for letter in "coffee":
+    print(letter)
+
+# -------
+# range()
+# -------
+
+# range(6) -> 0,1,2,3,4,5
+# range(1,7) -> 1,2,3,4,5,6
+# range(1,10,2) -> 1,3,5,7,9
+# range(7,0,-1) -> 7,6,5,4,3,2,1
+
+# ----------
+# while loop
+# ----------
+
+num = 1
+while num < 11:
+    print(num)
+    num += 1
--- s8_boolean_and_conditional_logic.py
+++ s8_boolean_and_conditional_logic.py
@@ -1,1 +1,23 @@
-# todo !
+# ------------------
+# Getting user input
+# ------------------
+
+print("What's your favorite color?")
+color_name = input()
+if color_name:
+    print(f"Your favorite color is {color_name} !")
+else:
+    print("You didn't say anything")
+
+# ----------------------
+# Conditional statements
+# ----------------------
+
+x = input("Enter a value for x: ")
+x = int(x)
+if x > 0 and x < 10:
+    print("x is larger than 0 and smaller thant 10")
+elif x >= 10:
+    print("x is larger or equal to 10")
+else:
+    print("x is negative")
